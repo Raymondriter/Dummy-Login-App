dbeb8ff2c5ca76b608cc167ae87497c97725ea3d
Raymondriter <rpr2998@gmail.com>
2021-07-06 00:42:12 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/apptest/ui/login/RegisterUser.java
@@ -0,0 +1,16 @@
+package com.example.apptest.ui.login;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.apptest.R;
+
+public class RegisterUser extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
